986540479d6425b4ecf098cd878cf740e1874940
Elioenay Silva <elionay407@gmail.com>
2023-10-15 22:33:57 -0300

    Refatorando Testes E2E

--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -6,10 +6,18 @@ import {
   QuestionComment,
   QuestionCommentProps,
 } from '@/domain/forum/enterprise/entities/question-comment'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import {
+  Question,
+  QuestionProps,
+} from '@/domain/forum/enterprise/entities/question'
+import { makeQuestion } from './make-question'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestionComment(
   override: Partial<QuestionCommentProps> = {},
-  id?: UniqueEntityID,
+  id?: UniqueEntityID
 ) {
   const question = QuestionComment.create(
     {
@@ -18,8 +26,25 @@ export function makeQuestionComment(
       content: faker.lorem.text(),
       ...override,
     },
-    id,
+    id
   )
 
   return question
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {}
+  ): Promise<Question> {
+    const questionComment = makeQuestion(data)
+
+    await this.prisma.comment.create({
+      data: PrismaQuestionMapper.toPrisma(questionComment),
+    })
+
+    return questionComment
+  }
 }
